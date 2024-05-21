c538011d1346dcc7f377fef4aa20f5ef3ab5f900
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2024-05-20 22:36:41 -0400

    Iniciando projeto usando Quasar =D

---
+++ quasar-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+
+    <q-header reveal class="bg-dark text-white" >
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="./assets/Mineração Candido - Logo Branca.svg" width="200px">
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+    </q-page-container>
+
+    <q-footer elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div></div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'App'
+});
+</script>
