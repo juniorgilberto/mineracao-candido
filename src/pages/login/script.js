e800e8d23ee83b1477a71ffee8843926bce686d9
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2025-03-09 22:48:06 -0400

    Atualizações Tela de Login e Tela Home
    Mudanças sutis que fazem a diferença na experiência do usuário e melhoramento do design das telas.

--- src/pages/login/script.js
+++ src/pages/login/script.js
@@ -1,6 +1,11 @@
 document.getElementById("loginForm").addEventListener("submit", async function (event) {
     event.preventDefault();
 
+    const loadingOverlay = document.getElementById("loadingOverlay");
+
+    // Exibe o overlay de carregamento
+    loadingOverlay.style.display = "flex";
+
     const userID = document.getElementById("userID").value;
     const userPassword = document.getElementById("userPassword").value;
 
@@ -26,10 +31,23 @@ document.getElementById("loginForm").addEventListener("submit", async function (
             // Redireciona para a Página HOME
             window.location.href = "../homepage/index.html";
         } else {
-            alert("ID ou senha inválidos!");
+            showErrorModal("Usuário ou senha inválidos!");
         }
     } catch (error) {
         console.error("Erro ao conectar à API:", error);
-        alert("Ocorreu um erro. Tente novamente mais tarde.");
+        showErrorModal("Erro ao conectar. Tente novamente.");
+    } finally {
+        loadingOverlay.style.display = "none";
     }
 });
+
+// Função para exibir o modal de erro
+function showErrorModal(message) {
+    document.getElementById("errorMessage").textContent = message;
+    document.getElementById("errorModal").style.display = "flex";
+}
+
+// Função para fechar o modal de erro
+function closeErrorModal() {
+    document.getElementById("errorModal").style.display = "none";
+}
