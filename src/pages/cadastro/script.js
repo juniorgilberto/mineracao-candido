db96aae8c58111c303171f044d2d4072aa4772a0
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2025-03-30 18:37:07 -0400

    Correções de responsividade e inicio da tela de cadastro

--- src/pages/cadastro/script.js
+++ src/pages/cadastro/script.js
@@ -14,3 +14,27 @@ function toggleMenu() {
         menu.style.display = "flex";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const tabelaBody = document.querySelector("#tabela-clientes tbody");
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        
+        const nome = document.getElementById("nome").value;
+        const cpfCnpj = document.getElementById("cpf_cnpj").value;
+        const inscricaoEstadual = document.getElementById("inscricao_estadual").value;
+        const telefone = document.getElementById("telefone").value;
+        const email = document.getElementById("email").value;
+        const endereco = document.getElementById("endereco").value;
+        
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `<td>${nome}</td><td>${cpfCnpj}</td><td>${inscricaoEstadual}</td><td>${telefone}</td><td>${email}</td><td>${endereco}</td>`;
+        tabelaBody.appendChild(newRow);
+        
+        form.reset();
+        var modal = bootstrap.Modal.getInstance(document.getElementById('clienteModal'));
+        modal.hide();
+    });
+});
