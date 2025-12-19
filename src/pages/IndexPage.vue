d7038571cfcc076939969a5d0c0ef40cf7bda7d9
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2025-12-19 18:11:22 -0400

    Agrupando os pedidos
    Agrupei os pedidos pelo cliente e listei os veiculos para os dividir em cards

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,22 +1,24 @@
 <template>
   <q-page padding>
-    <q-toolbar class="q-mb-md items-center">
+    <q-toolbar class="row q-col-gutter-md justify-around">
       <q-input dense outlined rounded placeholder="Pesquisar (cliente / placa / material...)" debounce="300"
-        class="q-mr-sm" style="max-width: 360px;">
+        class="q-mr-sm col-md-6" style="max-width: 360px;">
         <template v-slot:append>
           <q-icon name="search" />
         </template>
       </q-input>
-      <q-space />
-      <q-text class="text-subtitle1 text-grey q-mr-md">Hoje: {{ new Date().toLocaleDateString('pt-BR') }}</q-text>
-      <q-btn label="+ Nova Venda" color="positive" @click="card = true" />
+      <div class="row col-md-6 justify-end items-center">
+        <div class="text-subtitle1 text-grey q-mr-md">Hoje: {{ new Date().toLocaleDateString('pt-BR') }}</div>
+        <q-btn label="+ Nova Venda" color="positive" @click="card = true" />
+      </div>
+
     </q-toolbar>
 
 
     <q-dialog v-model="card">
       <q-card class="my-card">
         <q-form class="q-pa-lg">
-          <q-text class="text-h6">Nova Venda</q-text>
+          <div class="text-h6 q-pb-sm">Nova Venda</div>
           <q-select id="clienteSelect" outlined v-model="clientId" :options="clientes" option-value="id"
             option-label="nome" label="Cliente" emit-value map-options>
             <template v-slot:no-option>
@@ -32,19 +34,24 @@
 
           <div class="flex row q-gutter-x-md">
             <q-select outlined v-model="veiculoId" :options="veiculos" option-value="id" option-label="placa" emit-value
-              map-options :disable="!clientId" :loading="carregandoVeiculos" label="Veículo" class="q-mt-md"
-              style="width: 30%" />
-            <q-input label="ou digite nova placa" outlined type="text" class="q-mt-md" style="width: 60%" />
+              map-options :disable="!clientId" :loading="carregandoVeiculos" label="Placa do Veículo"
+              class="q-mt-md col-5">
+              <template v-slot:no-option>
+                <q-item>
+                  <q-item-section class="text-grey">
+                    Nenhum veículo cadastrado.
+                  </q-item-section>
+                </q-item>
+              </template>
+            </q-select>
+
+            <q-input label="ou digite nova placa" outlined type="text" class="q-mt-md col-6" />
 
           </div>
           <div class="row q-gutter-x-md">
-            <q-select outlined v-model="modelMaterial" label="Material" :options=material class="q-mt-md"
-              style="width: 60%" />
-            <q-input label="M³" outlined type="number" class="q-mt-md" style="width: 30%" />
+            <q-select outlined v-model="modelMaterial" label="Material" :options=material class="q-mt-md col-5" />
+            <q-input label="M³" outlined type="number" v-model="metrosCubicos" class="q-mt-md col-6" />
           </div>
-
-          <q-checkbox label="Salvar cliente/caminhão (se novo)" v-model="val" class="q-mt-md" />
-
           <q-separator />
 
           <q-btn class="q-ma-md" style="float: right;" label="Salvar Venda" type="submit" color="primary" />
@@ -52,36 +59,41 @@
       </q-card>
     </q-dialog>
 
-    <div class="row q-col-gutter-sm">
-      <q-card v-for="pedido in pedidos" :key="pedido.id" class="q-pa-md q-mb-md col-12 col-md-4" flat bordered>
-        <div class="text-bold text-primary text-h6">
-          {{ pedido.client.nome }}
+
+    <div v-if="!pedidosAgrupados.length" class="text-center text-grey-7 q-mt-xl">
+      Nenhuma venda lançada no dia de hoje. Clique em <b>Nova Venda</b> para cadastrar uma.
+    </div>
+    <div v-else class="row justify-center q-col-gutter-sm">
+      <q-card v-for="cliente in pedidosAgrupados" :key="cliente.nome" class="q-pa-md col-12 col-md-3 q-ma-md" flat
+        bordered>
+        <div class="text-h6 text-primary text-bold">
+          {{ cliente.nome }}
         </div>
 
         <div class="text-caption q-mb-sm">
-          1 caminhão(ões)
+          {{ Object.keys(cliente.veiculos).length }} caminhão(ões)
         </div>
 
         <q-separator />
 
-        <div class="q-mt-md row justify-between">
+        <div v-for="(v, placa) in cliente.veiculos" :key="placa" class="q-mt-md row justify-between">
           <div>
-            <div class="text-bold">{{ pedido.veiculo ? pedido.veiculo.placa : 'SEM PLACA DEFINIDA' }}</div>
-
+            <div class="text-bold">{{ placa }}</div>
             <div class="text-grey-7">
-              {{ pedido.produto.nome }} • {{ pedido.metragem }} m³
+              {{ v.produto }} • {{ v.metragem }} m³
             </div>
           </div>
-          <div class="row items-center justify-end ">
-            <q-badge color="blue-2 q-pa-sm q-mr-md" text-color="primary">
-              1 viagem(s)
+
+          <div class="row items-center">
+            <q-badge color="blue-2" text-color="primary" class="q-mr-md">
+              {{ v.viagens }} viagem(s)
             </q-badge>
-
-            <q-btn color="primary" size="sm" unelevated label="+ Viagem" />
+            <q-btn size="sm" color="primary" unelevated label="+ Viagem" />
           </div>
+          <q-separator />
         </div>
-
       </q-card>
+
     </div>
 
 
@@ -92,7 +104,7 @@
 <script setup>
 
 import { api } from 'src/boot/axios'
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted, computed } from 'vue'
 import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
@@ -102,9 +114,9 @@ const veiculos = ref([])
 const veiculoId = ref(null)
 const material = ref([])
 const pedidos = ref([])
+const metrosCubicos = ref(null)
 
 const carregandoVeiculos = ref(false)
-const val = ref(false)
 const card = ref(false)
 const modelMaterial = ref(null)
 
@@ -141,7 +153,6 @@ async function carregarMaterial() {
   }
 }
 
-const carregando = ref(false);
 
 async function carregarPedidos() {
   const hide = $q.loading.show();
@@ -150,6 +161,8 @@ async function carregarPedidos() {
     pedidos.value = await getPedidos();
   } finally {
     hide();
+    console.log(pedidos);
+
   }
 }
 
@@ -166,6 +179,36 @@ async function getPedidos() {
 
 }
 
+const pedidosAgrupados = computed(() => {
+  const clientes = {}
+
+  pedidos.value.forEach(p => {
+    const clienteId = p.client.id
+    const placa = p.veiculo?.placa || 'SEM PLACA'
+
+    if (!clientes[clienteId]) {
+      clientes[clienteId] = {
+        nome: p.client.nome,
+        veiculos: {}
+      }
+    }
+
+    if (!clientes[clienteId].veiculos[placa]) {
+      clientes[clienteId].veiculos[placa] = {
+        produto: p.produto.nome,
+        metragem: p.metragem,
+        viagens: 0
+      }
+    }
+
+    clientes[clienteId].veiculos[placa].viagens++
+  })
+  console.log(clientes);
+  return Object.values(clientes)
+
+
+})
+
 watch(clientId, (id) => {
   veiculoId.value = null
   veiculos.value = []
@@ -173,6 +216,16 @@ watch(clientId, (id) => {
   if (!id) return
 
   carregarVeiculos(id)
+})
+
+watch(veiculoId, (id) => {
+  if (!id) {
+    metrosCubicos.value = null
+    return
+  }
+
+  const veiculo = veiculos.value.find(v => v.id === id)
+  metrosCubicos.value = veiculo?.metragem || null
 })
 
 carregarCliente(),
